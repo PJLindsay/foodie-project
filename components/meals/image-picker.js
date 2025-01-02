d609de0f9b22096b90bd7f6e634fd1123a4f5f26
PJ Lindsay <pjlindsay@gmail.com>
2025-01-02 13:30:02 -0700

    Adjust ImagePicker to have custom look

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,10 +1,22 @@
+"use client";
+
+import { useRef } from "react";
 import classes from "./image-picker.module.css";
 
 export default function ImagePicker({ label, displayName }) {
+
+  const imageInput = useRef()
+
+  function handleImageSelectorClick() { 
+    imageInput.current.click();
+  }
+
   return <div className={classes.picker}>
     <label htmlFor={displayName}>{label}</label>
     <div className="control">
-      <input type="file" id={displayName} accept="image/png, image.jpeg" name={displayName} />
+      {/* note our classes.input css hides the default input */}
+      <input className={classes.input} type="file" id={displayName} accept="image/png, image.jpeg" name={displayName} ref={imageInput} />
+      <button className={classes.button} type="button" onClick={handleImageSelectorClick}>Choose an image</button>
     </div>
   </div>
 }
